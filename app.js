bafcc2f10177375afcedaae62282f24ae58b34b6
minipiglucy0215 <yoyo520yoyo520_yoyo520tw@yahoo.com.tw>
2017-07-20 22:22:53 +0800

    smallbutton

--- app.js
+++ app.js
@@ -336,7 +336,9 @@ function receivedMessage(event) {
       case 'account linking':
         sendAccountLinking(senderID);
         break;
-
+      case 'Smallbutton':
+        sendSmallbutton(senderID);
+        break;
       default:
         sendTextMessage(senderID, messageText);
     }
@@ -610,6 +612,65 @@ function sendButtonMessage(recipientId) {
       }
     }
   };  
+
+  callSendAPI(messageData);
+}
+
+function sendSmallbuttonMessage(recipientId) {
+  var messageData = {
+    "recipient": {
+    "id": recipientId
+  },
+  "message": {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [
+          {
+            "title": "Swipe left/right for more options.",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Button 1",
+                "payload": "button1"
+              },
+              {
+                "type": "postback",
+                "title": "Button 2",
+                "payload": "button2"
+              },
+              {
+                "type": "postback",
+                "title": "Button 3",
+                "payload": "button3"
+              }
+            ]
+          },
+          {
+            "title": "Swipe left/right for more options.",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Button 4",
+                "payload": "button4"
+              },
+              {
+                "type": "postback",
+                "title": "Button 5",
+                "payload": "button5"
+              },
+              {
+                "type": "postback",
+                "title": "Button 6",
+                "payload": "button6"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
 
   callSendAPI(messageData);
 }
